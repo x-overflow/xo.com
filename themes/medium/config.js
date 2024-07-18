ac3853c087bb4d4ef07c63d545161aa963c6ab90
xOverflow <the3body@outlook.com>
2024-07-18 16:26:24 +0800

    Update config.js

--- themes/medium/config.js
+++ themes/medium/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
 
   // Style
-  MEDIUM_RIGHT_PANEL_DARK: process.env.NEXT_PUBLIC_MEDIUM_RIGHT_DARK || false, // 右侧面板深色模式
+  MEDIUM_RIGHT_PANEL_DARK: process.env.NEXT_PUBLIC_MEDIUM_RIGHT_DARK || true, // 右侧面板深色模式
 
   MEDIUM_POST_LIST_COVER: true, // 文章列表显示图片封面
   MEDIUM_POST_LIST_PREVIEW: true, // 列表显示文章预览
